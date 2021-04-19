fd1426b736f62a591555a499f4f1189346615d63
Anton Karpenko <karpolan@karpolan.com>
2021-04-20 01:46:55 +0300

    feat: CRA with TypeScript feat: FavIcon refactor: App and tests feat: ReadMe fix: Version updated

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders KARPOLAN link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const copyrightLink = screen.getByText(/KARPOLAN/i);
+  expect(copyrightLink).toBeInTheDocument();
 });

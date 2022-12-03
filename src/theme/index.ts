48f363d3bd9306219675c4ae50619d6dd93e8893
Anton Karpenko <karpolan@karpolan.com>
2022-12-03 05:39:58 +0100

    feat: lint script

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,6 +1,14 @@
+import { Theme } from '@mui/material/styles';
 import AppThemeProvider from './AppThemeProvider';
 import DARK_THEME from './dark';
 import LIGHT_THEME from './light';
+
+declare module '@mui/styles/defaultTheme' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface DefaultTheme extends Theme {
+    // TODO: If you will extend Theme with own properties, also add custom variables here. See https://mui.com/material-ui/customization/theming/#custom-variables
+  }
+}
 
 export {
   LIGHT_THEME as default, // Change to DARK_THEME if you want to use dark theme as default
